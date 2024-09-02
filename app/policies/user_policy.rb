7311c2b3c08dddcce5c89cd50cf2842804df3302
Bruno Silva <111820238+BrunooIglesias@users.noreply.github.com>
2024-09-02 14:29:06 -0300

    feat: user policy

---
+++ app/policies/user_policy.rb
@@ -0,0 +1,31 @@
+class UserPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      if user.admin?
+        scope.all
+      else
+        scope.where(id: user.id)
+      end
+    end
+  end
+
+  def index?
+    user.admin?
+  end
+
+  def show?
+    user.admin? || record.id == user.id
+  end
+
+  def create?
+    user.admin?
+  end
+
+  def update?
+    user.admin? || record.id == user.id
+  end
+
+  def destroy?
+    user.admin?
+  end
+end
